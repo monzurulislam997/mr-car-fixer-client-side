eeb0e88562bde71022c2d8d42c283afa5032e5eb
fname lname <monzurulislam997@gmail.com>
2022-08-02 20:02:10 +0600

    firebase auth problem solved

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -20,21 +20,3 @@ export default auth;
 
 
 
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBB5ffTPH6jVyBazguu2_5FmU0we2D3kyc",
-//   authDomain: "mr-car-fixer.firebaseapp.com",
-//   projectId: "mr-car-fixer",
-//   storageBucket: "mr-car-fixer.appspot.com",
-//   messagingSenderId: "632577866694",
-//   appId: "1:632577866694:web:3d08f7ef715de573ebd0c7"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
